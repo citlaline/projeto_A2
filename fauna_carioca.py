c284cebeed316075b83ef77df09943d8c1d92e95
citlaline <168496446+citlaline@users.noreply.github.com>
2024-06-11 15:00:05 -0300

    Update fauna_carioca.py

--- fauna_carioca.py
+++ fauna_carioca.py
@@ -14,7 +14,7 @@ with col1:
 
 with col2:
     filtered_df = df[df['Municipio'] == option1]
-    unique_results = filtered_df['Nome cientifico'].drop_duplicates().sort_values()
+    unique_species = filtered_df['Nome cientifico'].drop_duplicates().sort_values()
     for species in unique_species:
         st.subheader(species)
         conservation_status = filtered_df[filtered_df['Nome cientifico'] == species]['Estado de conservacao'].iloc[0]
