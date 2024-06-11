ddce3fe83f49784b9f6d06c63dcd0ce91651c4c6
citlaline <168496446+citlaline@users.noreply.github.com>
2024-06-11 15:12:56 -0300

    Update fauna_carioca.py

--- fauna_carioca.py
+++ fauna_carioca.py
@@ -16,7 +16,7 @@ with col2:
     filtered_df = df[df['Municipio'] == option1]
     unique_species = filtered_df['Nome cientifico'].drop_duplicates().sort_values()
     for species in unique_species:
-        common_name = filtered_df[filtered_df['Nome cientifico'] == species]['Nome comum'].iloc[0]
+        common_name = filtered_df[filtered_df['Nome cientifico'] == species]['Nome comum'].iloc[0].lower()
         st.subheader(f"{species}")
         st.write("Nome comum: ",f"{common_name}")
         conservation_status = filtered_df[filtered_df['Nome cientifico'] == species]['Estado de conservacao'].iloc[0]
