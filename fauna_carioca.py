5b33759dc2899e4576bcd22dbce91f8813fc88af
citlaline <168496446+citlaline@users.noreply.github.com>
2024-06-14 10:15:31 -0300

    Update fauna_carioca.py

--- fauna_carioca.py
+++ fauna_carioca.py
@@ -22,11 +22,10 @@ with tab1:
 with tab2: 
     filtered_df = df[df['Municipio'] == option1]
     unique_names = filtered_df['Nome comum'].drop_duplicates().sort_values()
-    unique_names = filtered_df['Nome comum'].drop("Sem Informações")
+    unique_names = unique_names[unique_names != "Sem Informações"]
     for name in unique_names:
-        common_name = filtered_df[filtered_df['Nome cientifico'] == species]['Nome comum'].iloc[0].lower()
+        common_name = name.lower()  # Assuming you meant to use 'name' here
         st.subheader(f"{name}")
-
    
 
 
