9d899140639dc8c37a5837131b939cd9c7934354
citlaline <168496446+citlaline@users.noreply.github.com>
2024-06-18 14:14:51 -0300

    Update fauna_carioca.py

--- fauna_carioca.py
+++ fauna_carioca.py
@@ -56,5 +56,6 @@ with tab4:
     plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
     plt.axis('equal')
 
+    st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
 
